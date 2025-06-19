8533940d848613d5a3ee6bea0c2a9cb30204af7c
syjdjr <syjdjr@naver.com>
2025-06-19 15:51:34 +0900

    chore: springboot 기본 setting

---
+++ eolala/src/test/java/com/soongsil/eolala/EolalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongsil.eolala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EolalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
